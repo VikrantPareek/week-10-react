5b247682d2eaecf4c0975cf19c9dc937e706826e
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-04-25 17:39:23 +0530

    Assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 function App() {
-  let [count, setCount] = useState(0);
-  let countRef = useRef();
+  let foucsRef = useRef();
 
-  function startCount() {
-    if (countRef.current == null) {
-      countRef.current = setInterval(function () {
-        setCount((count) => count + 1);
-      }, 1000);
-    }
-  }
-
-  function stopCount() {
-    clearInterval(countRef.current);
-    countRef.current = null;
+  function handleFocus() {
+    foucsRef.current.focus();
   }
 
   return (
     <div>
-      {count}
-      <br />
-      <button onClick={startCount}>Start</button>
-      <button onClick={stopCount}>Stop</button>
+      <input ref={foucsRef} type="text" />
+      <button onClick={handleFocus}>Submit</button>
     </div>
   );
 }
 
 export default App;
+
+// import { useRef, useState } from "react";
+
+// function App() {
+//   let [count, setCount] = useState(0);
+//   let countRef = useRef();
+
+//   function startCount() {
+//     if (countRef.current == null) {
+//       countRef.current = setInterval(function () {
+//         setCount((count) => count + 1);
+//       }, 1000);
+//     }
+//   }
+
+//   function stopCount() {
+//     clearInterval(countRef.current);
+//     countRef.current = null;
+//   }
+
+//   return (
+//     <div>
+//       {count}
+//       <br />
+//       <button onClick={startCount}>Start</button>
+//       <button onClick={stopCount}>Stop</button>
+//     </div>
+//   );
+// }
+
+// export default App;
